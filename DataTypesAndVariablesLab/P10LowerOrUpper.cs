c559a1260ff5f0cedd218a52536c15610f371fe4
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2022-09-22 11:07:27 +0300

    Adding solutions

---
+++ DataTypesAndVariablesLab/P10LowerOrUpper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P10LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char inputChar = char.Parse(Console.ReadLine());
+
+            if ((int)inputChar >= 97)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
