b90119f4510a44d34df9d8c5d24e70a9d7b17b4e
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2022-11-19 19:06:47 +0200

    Upload solutions

---
+++ Regular Expressions - Lab/P01MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace P01MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z]{1}[a-z]{1,} [A-Z]{1}[a-z]{1,}\b";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
---
+++ Regular Expressions - Lab/P02MatchPhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace P02MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+           
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
